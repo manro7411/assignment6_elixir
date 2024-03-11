7155f5d31c712bce9f9ccd28ba9ae79f836c66d8
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-11 16:18:10 +0200

    Update -> Assignment6_comment

--- assignment_6/employee/lib/script.exs
+++ assignment_6/employee/lib/script.exs
@@ -1,16 +1,7 @@
-# script.exs
-
-# Require the content of the Employee module without evaluating it
 Code.require_file("employee1.ex", __DIR__)
-
-# Now that the Employee module is loaded, create a new employee
 nine_trai = Employee.new_employee(1000, "Ratchanon", "Traitiprat",:coder)
 IO.inspect nine_trai
-
-# Promote the employee and display the updated state
 nine_trai_promoted = Employee.promote(nine_trai)
 IO.inspect nine_trai_promoted
-
-# Demote the employee and display the updated state
 nine_trai_demoted = Employee.demote(nine_trai_promoted)
 IO.inspect nine_trai_demoted
